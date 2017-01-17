67119e1f6e7ce508fe2b77231c9e5c6e03b83efb
Kevin Nagurski <kevin.nagurski@brsgolf.com>
2017-01-17 23:31:08 +0000

    Add `CreateActivitiesTable` migration
    Adds a migration to create the `activities` table.
    
    Fields include:
    - `id` int(10) unsigned NOT NULL AUTO_INCREMENT
    - `user_id` int(10) unsigned NOT NULL
    - `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL
    - `created_at` timestamp NULL DEFAULT NULL
    - `updated_at` timestamp NULL DEFAULT NULL
    - `deleted_at` timestamp NULL DEFAULT NULL
    
    Primary key: `id`
    Foreign key: `user_id` => `users`.`id`
    
    Refs #1

---
+++ database/migrations/2017_01_17_231217_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('subject');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
